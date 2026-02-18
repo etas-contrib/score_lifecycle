6bcdeb8227ab6572a59900f0a1b96c2a2cd8e907
Empting Eelco (ETAS-ECM/XPC-Fe2) <Eelco.Empting2@etas.com>
2026-02-18 08:26:47 +0100

    Start work

--- scripts/config_mapping/lifecycle_config.py
+++ scripts/config_mapping/lifecycle_config.py
@@ -72,6 +72,8 @@ def preprocess_defaults(global_defaults, config):
     The resulting file with have no "defaults" entry anymore, but looks like if the user had specified all the fields explicitly.
     """
     def dict_merge(dict_a, dict_b):
+        if dict_b is None:
+            return dict_a
         for key, value in dict_b.items():
             if key in dict_a and isinstance(dict_a[key], dict) and isinstance(value, dict):
                 # For certain dictionaries, we do not want to merge the defaults with the user specified values
@@ -118,10 +120,11 @@ def preprocess_defaults(global_defaults, config):
         if run_target == "initial_run_target":
             new_config["run_targets"][run_target] = run_target_config
         else:
-            new_config["run_targets"][run_target] = dict_merge(merged_defaults["run_target"], run_target_config)
+            # Merge into a fresh copy so we don't mutate defaults or alias run target dicts.
+            new_config["run_targets"][run_target] = dict_merge(deepcopy(merged_defaults["run_target"]), run_target_config)
 
-    new_config["alive_supervision"] = dict_merge(merged_defaults["alive_supervision"], config.get("alive_supervision", {}))
-    new_config["watchdogs"] = dict_merge(merged_defaults["watchdogs"], config.get("watchdogs", {}))
+    new_config["alive_supervision"] = dict_merge(deepcopy(merged_defaults["alive_supervision"]), config.get("alive_supervision", {}))
+    new_config["watchdogs"] = dict_merge(deepcopy(merged_defaults["watchdogs"]), config.get("watchdogs", {}))
 
     #print(json.dumps(new_config, indent=4))
 
@@ -226,9 +229,106 @@ def gen_launch_manager_config(output_dir, config):
     Output:
     - A file named "lm_demo.json" containing the launch manager configuration
     """
+    def get_recovery_state():
+        name = config["run_targets"]["final_recovery_action"]["switch_run_target"]["run_target"]
+        return "MainPG/" + name
+    
+    def get_process_dependencies(run_target):
+        out = []
+        if not "depends_on" in run_target:
+            return out
+        for component in run_target["depends_on"]:
+            if component in config["components"]:
+                out.append(component)
+                if "depends_on" in config["components"][component]["component_properties"]:
+                    # All dependencies must be components, since components can't depend on run targets
+                    for dep in config["components"][component]["component_properties"]["depends_on"]:
+                        if not dep in out:
+                            out.append(dep)
+                            out += get_process_dependencies(config["components"][dep]["component_properties"])
+            else:
+                out += get_process_dependencies(config["run_targets"][component])
+        return out
+
+
+    lm_config = {}
+    lm_config["versionMajor"] = 7
+    lm_config["versionMinor"] = 0
+    lm_config["Process"] = []
+    lm_config["ModeGroup"] = [{
+        "identifier": "MainPG",
+        "initialMode_name": "Off",
+        "recoveryMode_name": get_recovery_state(),
+        "modeDeclaration": []
+    }]
+
+    process_group_states = {}
+
+    # Run targets can depend on components and on other run targets
+    for pgstate, values in config["run_targets"].items():
+        if pgstate in ["initial_run_target", "final_recovery_action"]:
+            continue
+        lm_config["ModeGroup"][0]["modeDeclaration"].append({
+            "identifier": "MainPG/" + pgstate
+        })
+        components = list(set(get_process_dependencies(values)))
+        state_name = "MainPG/" + pgstate
+        for component in components:
+            if component not in process_group_states:
+                process_group_states[component] = []
+            process_group_states[component].append(state_name)
+
+    import pdb
+    pdb.set_trace()
+
+    for component_name, component_config in config["components"].items():
+        process = {}
+        process["uid"] = component_config["deployment_config"]["sandbox"]["uid"]
+        process["gid"] = component_config["deployment_config"]["sandbox"]["gid"]
+        process["sgids"] = component_config["deployment_config"]["sandbox"]["supplementary_group_ids"]
+        process["identifier"] = component_name
+        process["path"] = component_config["deployment_config"]["bin_dir"] + "/" + component_config["component_properties"]["binary_name"]
+        process["numberOfRestartAttempts"] = component_config["deployment_config"]["ready_recovery_action"]["restart"]["number_of_attempts"]
+        process["startupConfig"] = [{}]
+        process["startupConfig"][0]["enterTimeoutValue"] = component_config["deployment_config"]["ready_timeout"]
+        process["startupConfig"][0]["exitTimeoutValue"] = component_config["deployment_config"]["shutdown_timeout"]
+        process["startupConfig"][0]["schedulingPolicy"] = component_config["deployment_config"]["sandbox"]["scheduling_policy"]
+        process["startupConfig"][0]["schedulingPriority"] = component_config["deployment_config"]["sandbox"]["scheduling_priority"]
+        process["startupConfig"][0]["processGroupStateDependency"] = []
+        process["startupConfig"][0]["environmentVariable"] = []
+        for env_var, value in component_config["deployment_config"]["environmental_variables"].items():
+            process["startupConfig"][0]["environmentVariable"].append({
+                "key": env_var,
+                "value": value
+            })
+
+        if "process_arguments" in component_config:
+            process["startupConfig"][0]["processArgument"] = component_config["process_arguments"]
+
+        if component_name in process_group_states:
+            for pgstate in process_group_states[component_name]:
+                process["startupConfig"][0]["processGroupStateDependency"].append({
+                    "stateMachine_name": "MainPG",
+                    "stateName": pgstate
+                })
+
+        lm_config["Process"].append(process)
+    
+    # Components can never depend on run targets
+    for process in lm_config["Process"]:
+        process["executionDependency"] = []
+        for dependency in config["components"][process["identifier"]]["component_properties"]["depends_on"]:
+            # import pdb
+            # pdb.set_trace()
+            dep_entry = config["components"][dependency]
+            ready_condition = dep_entry["component_properties"]["ready_condition"]
+            process["executionDependency"].append({
+                "stateName": ready_condition,
+                "targetProcess_identifier": f"/{dependency}App/{dependency}"
+            })
+        
     
     with open(f"{output_dir}/lm_demo.json", "w") as lm_file:
-        lm_config = {}
         json.dump(lm_config, lm_file, indent=4)
 
 def main():

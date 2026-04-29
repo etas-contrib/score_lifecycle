31eab761ecdcca79fe2620ef00930fa6c507d543
William Roebuck <244554584+WilliamRoebuck@users.noreply.github.com>
2026-04-29 13:50:28 +0100

    Reproduce issue

---
+++ tests/integration/incorrect_config_no_comms/no_comms_process.cpp
@@ -0,0 +1,39 @@
+/********************************************************************************
+ * Copyright (c) 2026 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+
+#include <fcntl.h>
+#include <gtest/gtest.h>
+#include <score/lcm/lifecycle_client.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include "tests/utils/test_helper/test_helper.hpp"
+#include <csignal>
+
+TEST(NoComms, Process)
+{
+    auto fd = shm_open("some_shared_memory", O_CREAT | O_RDWR, 0);
+    shm_unlink("some_shared_memory");
+
+    // report kRunning
+    auto result = score::lcm::LifecycleClient{}.ReportExecutionState(score::lcm::ExecutionState::kRunning);
+    EXPECT_FALSE(result.has_value()) << "client.ReportExecutionState() failed";
+
+    close(fd);
+}
+
+int main()
+{
+    return TestRunner(__FILE__).RunTests();
+}
---
+++ tests/integration/incorrect_config_no_comms/test_incorrect_config_no_comms.py
@@ -0,0 +1,41 @@
+# *******************************************************************************
+# Copyright (c) 2026 Contributors to the Eclipse Foundation
+#
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Apache License Version 2.0 which is available at
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# SPDX-License-Identifier: Apache-2.0
+# *******************************************************************************
+import logging
+from tests.utils.testing_utils.run_until_file_deployed import run_until_file_deployed
+from tests.utils.testing_utils.setup_test import setup_test
+from tests.utils.testing_utils.test_results import (
+    check_for_failures,
+    download_xml_results,
+)
+from attribute_plugin import add_test_properties
+
+
+@add_test_properties(
+    partially_verifies=[],
+    test_type="interface-test",
+    derivation_technique="explorative-testing",
+)
+def test_incorrect_config_no_comms(target, setup_test, test_output_dir, remote_test_dir):
+    """Test behavior when a non-reporting process attempts to report running"""
+    run_until_file_deployed(
+        target=target,
+        binary_path=str(remote_test_dir / "launch_manager"),
+        file_path=remote_test_dir.parent / "test_end",
+        cwd=str(remote_test_dir),
+        timeout_s=2.0,
+    )
+
+    download_xml_results(target, remote_test_dir, test_output_dir)
+    all_files, failing_files = check_for_failures(test_output_dir)
+    assert len(all_files) == 2, f"Didn't find the expected number of files {all_files}"
+    assert len(failing_files) == 0, f"Found failures in files {failing_files}"
